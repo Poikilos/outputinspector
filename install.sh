8c9c686bd62561a00e9d54d0b9ac5caf3ee1fd08
poikilos <7557867+poikilos@users.noreply.github.com>
2021-02-12 06:51:51 -0500

    Make install.sh output more condensed and uniform.

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ echo
 echo "Installing $PROJECT_NAME..."
 echo
 echo
-echo "looking for instances to terminate..."
+printf "* looking for instances to terminate..."
 killall $BIN_FILE_NAME
 
-echo "Checking environment:"
+printf "* checking environment..."
 if [ -z "$PREFIX" ]; then
     if [ "@$USER" == "@root" ]; then
         PREFIX=/usr/local
@@ -43,67 +43,68 @@ BIN_DEST_DIR=$PREFIX/bin
 #remember, project-specific info is also at end of this script (post-install notes)
 BIN_CONFLICT_DIR=/usr/bin
 if [ -f "$BIN_CONFLICT_DIR/$BIN_FILE_NAME" ]; then
-  echo
-  echo
-  echo "You are installing the source version of $PROJECT_NAME, so you must first remove the packaged version of $PROJECT_NAME to avoid conflicting file '$BIN_CONFLICT_DIR/$BIN_FILE_NAME'--if you used an earlier version of $PROJECT_NAME, you may have this file from a installer that was not yet up to standards so before running install again as root you will have to run:"
-  echo "su root"
-  echo "rm -f /usr/bin/$BIN_FILE_NAME"
-  echo
-  echo
-  exit 1
+    echo
+    echo
+    echo "You are installing the source version of $PROJECT_NAME, so you must first remove the packaged version of $PROJECT_NAME to avoid conflicting file '$BIN_CONFLICT_DIR/$BIN_FILE_NAME'--if you used an earlier version of $PROJECT_NAME, you may have this file from a installer that was not yet up to standards so before running install again as root you will have to run:"
+    echo "su root"
+    echo "rm -f /usr/bin/$BIN_FILE_NAME"
+    echo
+    echo
+    exit 1
 fi
 
 uhoh="is in"
 DEBUG_DIR="../build-$PROJECT_NAME-Desktop-Debug"
 RELEASE_DIR="../build-$PROJECT_NAME-Desktop-Release"
 if [ -f "outputinspector" ]; then
-  #echo "ERROR: Nothing done for install since there is an ambiguous file named outputinspector here (it should instead be in '$RELEASE_DIR' or '$DEBUG_DIR')"
-  #exit 1
-  echo "WARNING: install found an ambiguous file named outputinspector here (it should instead be in '$RELEASE_DIR' or '$DEBUG_DIR')"
-  sMove="no"
-  #echo "Do you want to move it to Releases and try to continue (y/N)? "
-  #read sMove
-  sMove="yes"
-  if [ "${sMove:0:1}" = "y" ]; then
-    if [ ! -d "$RELEASE_DIR" ]; then
-      mkdir -p "$RELEASE_DIR"
+    #echo "ERROR: Nothing done for install since there is an ambiguous file named outputinspector here (it should instead be in '$RELEASE_DIR' or '$DEBUG_DIR')"
+    #exit 1
+    echo "WARNING: install found an ambiguous file named outputinspector here (it should instead be in '$RELEASE_DIR' or '$DEBUG_DIR')"
+    sMove="no"
+    #echo "Do you want to move it to Releases and try to continue (y/N)? "
+    #read sMove
+    sMove="yes"
+    if [ "${sMove:0:1}" = "y" ]; then
+        if [ ! -d "$RELEASE_DIR" ]; then
+            mkdir -p "$RELEASE_DIR"
+        fi
+        mv -f outputinspector "$RELEASE_DIR/"
+    else
+        exit 2
     fi
-    mv -f outputinspector "$RELEASE_DIR/"
-  else
-    exit 2
-  fi
 fi
 SOURCE_BIN_DIR="$RELEASE_DIR"
+printf "* looking for a built binary..."
 if [ ! -f "$RELEASE_DIR/$BIN_FILE_NAME" ]; then
-  SOURCE_BIN_DIR="$DEBUG_DIR"
-  if [ ! -f "$DEBUG_DIR/$BIN_FILE_NAME" ]; then
-    if [ -f "./$BIN_FILE_NAME" ]; then
-      SOURCE_BIN_DIR=.
-      echo "Using only copy of $BIN_FILE_NAME which is in current directory '.'"
+    SOURCE_BIN_DIR="$DEBUG_DIR"
+    if [ ! -f "$DEBUG_DIR/$BIN_FILE_NAME" ]; then
+        if [ -f "./$BIN_FILE_NAME" ]; then
+            SOURCE_BIN_DIR=.
+            echo "Using only copy of $BIN_FILE_NAME which is in current directory '.'"
+        else
+            echo
+            echo "Could not find '$DEBUG_DIR/$BIN_FILE_NAME' nor '$RELEASE_DIR/$BIN_FILE_NAME' so cannot install--$PROJECT_NAME.pro must be first built using Qt Creator 5, install.sh, or the binary must be built by other means and placed in one of those folders or the same folder as this script."
+            echo
+            echo
+            exit 2
+        fi
     else
-      echo
-      echo "Could not find '$DEBUG_DIR/$BIN_FILE_NAME' nor '$RELEASE_DIR/$BIN_FILE_NAME' so cannot install--$PROJECT_NAME.pro must be first built using Qt Creator 5, install.sh, or the binary must be built by other means and placed in one of those folders or the same folder as this script."
-      echo
-      echo
-      exit 2
+        echo "WARNING: Found \"$DEBUG_DIR/$BIN_FILE_NAME\" (but not \"$RELEASE_DIR/$BIN_FILE_NAME\") so installing debug version"
     fi
-  else
-    echo "WARNING: Found '$DEBUG_DIR/$BIN_FILE_NAME' (but not '$RELEASE_DIR/$BIN_FILE_NAME') so installing debug version"
-  fi
 else
-  echo "Found '$RELEASE_DIR/$BIN_FILE_NAME'"
+    echo "FOUND (\"$RELEASE_DIR/$BIN_FILE_NAME\")"
 fi
 
 #if both exist, do not use one that is -ot (older than) other one
 if [ -f "$RELEASE_DIR/$BIN_FILE_NAME" ]; then
-  if [ -f "$DEBUG_DIR/$BIN_FILE_NAME" ]; then
-    if [ "$DEBUG_DIR/$BIN_FILE_NAME" -ot "$RELEASE_DIR/$BIN_FILE_NAME" ]; then
-      SOURCE_BIN_DIR="$RELEASE_DIR"
-      echo "Using newer install source in '$RELEASE_DIR'"
-    else
-      echo "WARNING: Using newer install source in '$DEBUG_DIR'"
+    if [ -f "$DEBUG_DIR/$BIN_FILE_NAME" ]; then
+        if [ "$DEBUG_DIR/$BIN_FILE_NAME" -ot "$RELEASE_DIR/$BIN_FILE_NAME" ]; then
+            SOURCE_BIN_DIR="$RELEASE_DIR"
+            echo "Using newer install source in '$RELEASE_DIR'"
+        else
+            echo "WARNING: Using newer install source in '$DEBUG_DIR'"
+        fi
     fi
-  fi
 fi
 BIN_FILE_PATH=$SOURCE_BIN_DIR/$BIN_FILE_NAME
 #cp -f ./bin/$BIN_FILE_NAME $BIN_DEST_DIR/
@@ -112,8 +113,6 @@ BIN_DEST_FILE_PATH=$BIN_DEST_DIR/$BIN_FILE_NAME
 if [ -f "$BIN_DEST_FILE_PATH" ]; then
     uhoh="was already installed: "
 fi
-
-echo
 
 was_there=false
 if [ -f "$BIN_DEST_DIR/$BIN_FILE_NAME" ]; then
@@ -138,87 +137,115 @@ if [ "@$was_there" = "@true" ]; then
         echo "OK"
     fi
 fi
-echo "cp -f \"$BIN_FILE_PATH\" \"$BIN_DEST_DIR/\""
+printf "* installing the binary to $BIN_DEST_DIR/..."
 cp -f "$BIN_FILE_PATH" "$BIN_DEST_DIR/"
 if [ ! -f "$BIN_DEST_DIR/$BIN_FILE_NAME" ]; then
-  echo
-  echo
-  #this error is always accurate since program would have exited already if file were already there before the cp command and couldn't be deleted:
-  echo "ERROR: install could not create '$BIN_DEST_DIR/$BIN_FILE_NAME'."
-  echo
-  echo
-  exit 4
-fi
-echo
-
-sCreate="no"
-sExistNote="changed"
-outputconf="/etc/$CONF_NAME"
-if [ -f $outputconf ]; then
-  sCreate="n"
-  # echo "Do you want to create a new $outputconf[y/N]?"
+    echo
+    echo
+    #this error is always accurate since program would have exited already if file were already there before the cp command and couldn't be deleted:
+    echo "ERROR: install could not create '$BIN_DEST_DIR/$BIN_FILE_NAME'. via 'cp -f \"$BIN_FILE_PATH\" \"$BIN_DEST_DIR/\"'"
+    echo
+    echo
+    exit 4
 else
-  sExistNote="created"
-  sCreate="y"
-  # echo "Do you want to create a new $outputconf[Y/n]?"
+    echo "OK"
 fi
 
+if [ -z "$RESET_CONF" ]; then
+    RESET_CONF="no"
+    sExistNote="changed"
+    outputconf="/etc/$CONF_NAME"
+    if [ -f $outputconf ]; then
+        RESET_CONF="n"
+        # echo "Do you want to create a new $outputconf[y/N]?"
+    else
+        sExistNote="created"
+        RESET_CONF="y"
+        # echo "Do you want to create a new $outputconf[Y/n]?"
+    fi
+fi
 
-# read sCreate
-
-if [ "${sCreate:0:1}" = "y" ]; then
-echo
-echo
-echo "Resetting $outputconf"
-#rm $outputconf
-kate_path=$(command -v kate)
-geany_path=$(command -v geany)
-if [ -f "$kate_path" ]; then
-  echo "kate=$kate_path" > $outputconf
-elif [ -f "$geany_path" ]; then
-  echo "kate=$geany_path" > $outputconf
+# read RESET_CONF
+printf "* detecting $outputconf..."
+# if [ "${RESET_CONF:0:1}" = "y" ]; then
+if [ "@$RESET_CONF" = "@y" ] || [ "@$RESET_CONF" = "@yes" ]; then
+    echo
+    echo
+    echo "FOUND"
+    echo "  * resetting $outputconf..."
+    # rm $outputconf
+    kate_path=$(command -v kate)
+    geany_path=$(command -v geany)
+    if [ -f "$kate_path" ]; then
+        echo "kate=$kate_path" > $outputconf
+    elif [ -f "$geany_path" ]; then
+        echo "kate=$geany_path" > $outputconf
+    else
+        end_error="WARNING: path to kate could not be found. Kate or Geany must be installed in order for this program to work."
+        echo $end_error
+        echo "kate=/usr/bin/kate" > $outputconf
+    fi
+    # echo "kate=/usr/lib/kde4/bin/kate" >> $outputconf
+    # echo "xEditorOffset=-1" >> $outputconf
+    # echo "yEditorOffset=-1" >> $outputconf
+    echo "ExitOnNoErrors=no" >> $outputconf
+    echo "FindTODOs=yes" >> $outputconf
+    echo "Kate2TabWidth=8" >> $outputconf
+    echo "CompilerTabWidth=6" >> $outputconf
+    echo "ShowWarningsLast=yes" >> $outputconf
+    echo
+    echo "    The owner of this file is: `stat -c '%U' install.sh`"
+    echo
+    echo "  * result (to change, edit $outputconf):"
+    chmod a+rw $outputconf
+    cat $outputconf
+    echo
+    echo
 else
-  end_error="WARNING: path to kate could not be found. Kate or Geany must be installed in order for this program to work."
-  echo $end_error
-  echo "kate=/usr/bin/kate" > $outputconf
-fi
-#echo "kate=/usr/lib/kde4/bin/kate" >> $outputconf
-#echo "xEditorOffset=-1" >> $outputconf
-#echo "yEditorOffset=-1" >> $outputconf
-echo "ExitOnNoErrors=no" >> $outputconf
-echo "FindTODOs=yes" >> $outputconf
-echo "Kate2TabWidth=8" >> $outputconf
-echo "CompilerTabWidth=6" >> $outputconf
-echo "ShowWarningsLast=yes" >> $outputconf
-echo
-echo "(you need root permissions to use this file)"
-echo
-echo "Result (to change, edit $outputconf):"
-chmod a+rw $outputconf
-cat $outputconf
-echo
-echo
-else
-echo "$outputconf not $sExistNote"
+    echo "$outputconf not $sExistNote since it exists (and RESET_CONF is neither 'yes' nor 'y')"
+    # ^ such as /etc/outputinspector.conf not changed
 fi
 
+printf "* verifying binary..."
 if [ ! -f "$BIN_FILE_PATH" ]; then
-  echo "$BIN_FILE_PATH not found!"
+    echo "ERROR: \"$BIN_FILE_PATH\" not found!"
 else
-   echo "found $BIN_FILE_PATH"
+    echo "OK (found \"$BIN_FILE_PATH\")"
 fi
+
+printf "* checking for existing $BIN_DEST_FILE_PATH..."
 
 if [ -f "$BIN_DEST_FILE_PATH" ]; then
-  echo "$BIN_FILE_NAME $uhoh $BIN_DEST_FILE_PATH"
-  chmod a+x "$BIN_DEST_FILE_PATH"
+    printf "$BIN_FILE_NAME $uhoh $BIN_DEST_FILE_PATH..."
+    chmod a+x "$BIN_DEST_FILE_PATH"
+    if [ $? -ne 0 ]; then
+        echo "FAILED ('chmod a+x \"$BIN_DEST_FILE_PATH\"')"
+    else
+        echo "OK"
+    fi
+else
+    echo "OK ($BIN_DEST_FILE_PATH does not exist--this is not a problem)"
 fi
 
+printf "* checking for $SRC_BINS_PATH..."
 if [ -d "$SRC_BINS_PATH" ]; then
-  chmod a+x $SRC_BINS_PATH/*
-  cp $SRC_BINS_PATH/* $BIN_DEST_DIR/
+    printf "setting permissions..."
+    chmod a+x $SRC_BINS_PATH/*
+    if [ $? -ne 0 ]; then
+        echo "FAILED ('chmod a+x $SRC_BINS_PATH/*' in `pwd`)"
+    else
+        echo "OK"
+    fi
+    printf "  * copying to $BIN_DEST_DIR/..."
+    cp $SRC_BINS_PATH/* $BIN_DEST_DIR/
+    if [ $? -ne 0 ]; then
+        echo "FAILED ('cp $SRC_BINS_PATH/* $BIN_DEST_DIR/' in `pwd`)"
+    else
+        echo "OK"
+    fi
 fi
 # else
-#   echo "WARNING: missing '$SRC_BINS_PATH'"
+#     echo "WARNING: missing '$SRC_BINS_PATH'"
 
 printf "* creating \"$PIXMAPS\"..."
 mkdir -p "$PIXMAPS"
@@ -234,9 +261,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-#TMP_ICON="/tmp/$ICON"
-#printf "* writing icon $TMP_ICON..."
-#cat > "$TMP_ICON" <<END
+# TMP_ICON="/tmp/$ICON"
+# printf "* writing icon $TMP_ICON..."
+# cat > "$TMP_ICON" <<END
 
 printf "* writing icon $DST_SC_PATH..."
 cat > "$DST_SC_PATH" <<END
@@ -280,50 +307,54 @@ else
     echo "  OK"
 fi
 
-echo
-echo
-echo "TROUBLESHOOTING"
-echo "If you have trouble with a release binary, open"
-echo ".pro file in Qt Creator and build $BIN_FILE_NAME"
-echo
-echo "USAGE AFTER SUCCESSFUL INSTALL"
-echo "In terminal, in project folder run something like:"
-echo "     mcs AssemblyInfo.cs MainForm.cs 2>err.txt"
-echo "     $BIN_FILE_NAME"
-echo "     or"
-echo "     jshint foo.js > err.txt"
-echo "     $BIN_FILE_NAME"
-echo " or your compiler or jshint output has"
-echo " to be otherwise redirected to err.txt,"
-echo " then run with that working directory:"
-echo "     $BIN_FILE_NAME"
-echo " from the same folder as the source code files and err.txt."
-echo
-echo "Then $BIN_FILE_NAME will automatically get working directory and"
-echo " send the absolute path to kate upon double-clicking a line"
-echo " from the error log using the outputinspector window."
-echo
-echo "DEVELOPMENT: err.txt (which is default, or file specified as"
-echo " first param of outputinspector)"
-echo " must be formatted exactly like one of these"
-echo " (the purpose of $BIN_FILE_NAME is to tell Kate to go"
-echo " to the file and location of warnings/errors such as):"
-echo "   foo.js: line 32, col 26, reason"
-echo "     or"
-echo "   foo.cs(10,24): reason"
+cat <<END
+
+
+TROUBLESHOOTING
+If you have trouble with a release binary, open
+.pro file in Qt Creator and build $BIN_FILE_NAME
+
+USAGE AFTER SUCCESSFUL INSTALL
+In terminal, in project folder run something like:
+     mcs AssemblyInfo.cs MainForm.cs 2>err.txt
+     $BIN_FILE_NAME
+     or
+     jshint foo.js > err.txt
+     $BIN_FILE_NAME
+ or your compiler or jshint output has
+ to be otherwise redirected to err.txt,
+ then run with that working directory:
+     $BIN_FILE_NAME
+ from the same folder as the source code files and err.txt.
+
+Then $BIN_FILE_NAME will automatically get working directory and
+ send the absolute path to kate upon double-clicking a line
+ from the error log using the outputinspector window.
+
+DEVELOPMENT: err.txt (which is default, or file specified as
+ first param of outputinspector)
+ must be formatted exactly like one of these
+ (the purpose of $BIN_FILE_NAME is to tell Kate to go
+ to the file and location of warnings/errors such as):
+   foo.js: line 32, col 26, reason
+     or
+   foo.cs(10,24): reason
+END
 if [ ! -z "$end_error" ]; then
-  #echo "WARNING: path to kate could not be found--install it or set the kate variable in $outputconf to full path of another binary which accepts -l <line> and -c <column> parameters."
-  echo "$end_error"
+    # echo "WARNING: path to kate could not be found--install it or set the kate variable in $outputconf to full path of another binary which accepts -l <line> and -c <column> parameters."
+    echo "$end_error"
 else
-  #NOTE: this is accurate since would have exited already if couldn't edit
-  echo "The $BIN_FILE_NAME is successfully installed at $BIN_DEST_DIR."
-  echo "NOTE: you can also use:"
-  echo "  editor=/usr/bin/geany"
-  echo "  # or editor=/usr/bin/kate"
-  echo "  # in '\$HOME/.local/share/outputinspector/settings.txt'"
-  echo "  # (outputinspector knows how to tell Geany which line"
-  echo "  # and column for setting your cursor position like with Kate)"
-  echo "See above for other information."
+    # NOTE: this is accurate since would have exited already if couldn't edit
+    cat <<END
+The $BIN_FILE_NAME is successfully installed at $BIN_DEST_DIR.
+NOTE: you can also use:
+  editor=/usr/bin/geany
+  # or editor=/usr/bin/kate
+  # in '\$HOME/.local/share/outputinspector/settings.txt'
+  # (outputinspector knows how to tell Geany which line
+  # and column for setting your cursor position like with Kate)
+See above for other information.
+END
 fi
 echo
 echo
